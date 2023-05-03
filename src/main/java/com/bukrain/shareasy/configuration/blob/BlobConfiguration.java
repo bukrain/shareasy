9f1eede06626a710b9746b92ee76b1d7778c0865
bukrain <bukrain1@gmail.com>
2023-05-03 16:34:27 +0200

    Add IdGenerator interface and implementation class

--- src/main/java/com/bukrain/shareasy/configuration/blob/BlobConfiguration.java
+++ src/main/java/com/bukrain/shareasy/configuration/blob/BlobConfiguration.java
@@ -5,6 +5,7 @@ import com.bukrain.shareasy.blob.facade.BlobFacadeImpl;
 import com.bukrain.shareasy.blob.service.BlobService;
 import com.bukrain.shareasy.blob.service.BlobServiceImpl;
 import com.bukrain.shareasy.expiration.service.ExpirationService;
+import com.bukrain.shareasy.helpers.id.IdGenerator;
 import com.bukrain.shareasy.query.repository.blob.BlobMetadataRepository;
 import com.bukrain.shareasy.query.repository.blob.BlobRepository;
 import com.bukrain.shareasy.webapi.blob.model.BlobToBlobModelConverter;
@@ -12,19 +13,31 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@SuppressWarnings("unused")
 public class BlobConfiguration {
 
     @Bean
-    public BlobFacade blobFacade(BlobToBlobModelConverter converter, BlobService service, ExpirationService expirationService) {
+    @SuppressWarnings("unused")
+    public BlobFacade blobFacade(
+            BlobToBlobModelConverter converter,
+            BlobService service,
+            ExpirationService expirationService
+    ) {
         return new BlobFacadeImpl(converter, service, expirationService);
     }
 
     @Bean
-    public BlobService blobService(BlobRepository blobRepository, BlobMetadataRepository blobMetadataRepository){
-        return new BlobServiceImpl(blobRepository, blobMetadataRepository);
+    @SuppressWarnings("unused")
+    public BlobService blobService(
+            BlobRepository blobRepository,
+            BlobMetadataRepository blobMetadataRepository,
+            IdGenerator idGenerator
+    ){
+        return new BlobServiceImpl(blobRepository, blobMetadataRepository, idGenerator);
     }
 
     @Bean
+    @SuppressWarnings("unused")
     public BlobToBlobModelConverter blobToBlobModelConverter(){
         return new BlobToBlobModelConverter();
     }
---
+++ src/main/java/com/bukrain/shareasy/helpers/HelpersConfiguration.java
@@ -0,0 +1,17 @@
+package com.bukrain.shareasy.helpers;
+
+import com.bukrain.shareasy.helpers.id.IdGenerator;
+import com.bukrain.shareasy.helpers.id.UUIDGenerator;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@SuppressWarnings("unused")
+public class HelpersConfiguration {
+
+    @Bean
+    @SuppressWarnings("unused")
+    public IdGenerator uuidGenerator(){
+        return new UUIDGenerator();
+    }
+}
